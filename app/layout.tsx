f4cdcba0a7c06c0c155b955fde7fda7f99b6e20c
MostakemHossain <mostakememon123@gmail.com>
2025-03-16 23:17:40 +0600

    project initilization

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Source_Sans_3 as FontSans } from "next/font/google";
+import "./globals.css";
+
+const fontSans = FontSans({
+  variable: "--font-sans",
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "Summatrix - AI-Powered PDF summarize",
+  description:
+    "Summatrix is an AI-Powered PDF summarize tool. It uses machine learning to summarize PDFs and extract key information.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${fontSans.variable} font-sans  antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
